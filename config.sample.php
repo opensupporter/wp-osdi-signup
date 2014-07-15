c494a6128539346eeaa6ae9cfba34b8aafc45b6f
Josh Cohen <joshco@gmail.com>
2014-07-15 06:01:45 -0400

    update sample config

--- config.sample.php
+++ config.sample.php
@@ -9,30 +9,45 @@
 
 
 $osdi_config = array(
-    // An optional enhancer service.  Before pushing signups into CRMs, first call the enhancer to get mode data
-    "enhancer" => array(
-        "name" => "TargetSmart / Catalist",
-        "url" => "http://tscat/proxy/tscat/enhance",
-        "api_token" => "<API-TOKEN>"
-    ),
+    //
     // A list of OSDI server endpoints to push signup information to
+    // Contact your vendor to obtain the relevant OSDI endpoint and credentials.
+    //
     "servers" => array(
         array(
             "name" => "Salsa",
             "url" => "http://salsaosdi/proxy/salsa/person_signup_helper",
-            "api_token" => "<API-TOKEN>"
+            "api_token" => "<API-TOKEN>",
+            "enabled" => true
         ),
         array(
             "name" => "VAN",
             "url" => "http://vanosdi/proxy/van/person_signup_helper",
-            "api_token" => "<API-TOKEN>"
+            "api_token" => "<API-TOKEN>",
+            "enabled" => true
         ),
         array(
             "name" => "BroadStripes",
             "url" => "https://crm.broadstripes.com/api/osdi/person_signup_helper",
-            "api_token" => "<API-TOKEN>"
+            "api_token" => "<API-TOKEN>",
+            "enabled" => true
         )
-    )
+    ),
+    "redirect" => array(
+        // Where to redirect the browser after submission
+        "success_url" => "http://localhost:3000/api/v1",
+        "fail_url" => "http://localhost/"
+    ),
+    //
+    // EXPERIMENTAL
+    // An optional enhancer service.  Before pushing signups into CRMs, first call the enhancer to get mode data
+    //
+    "enhancer" => array(
+        "name" => "TargetSmart / Catalist",
+        "url" => "http://tscat/proxy/tscat/enhance",
+        "api_token" => "<API-TOKEN>",
+        "enabled" => true
+    ),
 
 );
 ?>
